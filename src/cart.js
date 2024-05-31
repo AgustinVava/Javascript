42e25f37f2ba3ff740a0a328cab0d19a4763cc29
Agustin Vava <agustinvava20@gmail.com>
2024-05-30 22:57:04 -0300

    ultimos detalles

--- src/cart.js
+++ src/cart.js
@@ -11,7 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
   function mostrarCarrito() {
     const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
     cartItemsDiv.innerHTML = "";
-    carrito.forEach((item) => {
+    carrito.forEach((item, index) => {
+      
       const itemDiv = document.createElement("div");
       itemDiv.classList.add("bg-white", "p-4", "rounded-lg", "shadow-md");
 
@@ -23,8 +24,30 @@ document.addEventListener("DOMContentLoaded", function () {
       precio.textContent = `$${item.precio}`;
       precio.classList.add("text-gray-700", "mb-2");
 
+      // Botón de eliminar
+      const eliminarBtn = document.createElement("button");
+      eliminarBtn.textContent = "Eliminar";
+      eliminarBtn.classList.add(
+        "bg-red-500",
+        "text-white",
+        "px-4",
+        "py-2",
+        "rounded-md",
+        "hover:bg-red-600",
+        "focus:outline-none",
+        "focus:border-red-700",
+        "focus:ring",
+        "focus:ring-red-200",
+        "focus:ring-opacity-50",
+        "mb-2"
+      );
+      eliminarBtn.addEventListener("click", function () {
+        eliminarDelCarrito(index); 
+      });
+
       itemDiv.appendChild(nombre);
       itemDiv.appendChild(precio);
+      itemDiv.appendChild(eliminarBtn);
 
       cartItemsDiv.appendChild(itemDiv);
     });
@@ -34,5 +57,12 @@ document.addEventListener("DOMContentLoaded", function () {
     localStorage.removeItem("carrito");
     alert("¡Gracias por tu compra!");
     window.location.href = "./index.html";
+  }
+
+  function eliminarDelCarrito(index) {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    carrito.splice(index, 1); 
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito(); 
   }
 });
